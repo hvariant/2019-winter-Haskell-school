b5b5791786b3a40cd55330890039d5bf902627cc
Zhansong Li <lizhansong@hvariant.com>
2019-09-12 22:45:12 +1000

    fix warnings

--- cis194/week6/zhansongl/Fibonacci.hs
+++ cis194/week6/zhansongl/Fibonacci.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# OPTIONS_GHC -fno-warn-missing-methods #-}
 
@@ -13,7 +14,7 @@ fibs1 = map fib [0..]
 
 fibs2 :: [Integer]
 fibs2 = next 0 1
-  where next x y = x : next y (x+y)
+  where next a b = a : next b (a+b)
 
 data Stream a = Cons a (Stream a)
 
@@ -24,7 +25,7 @@ instance Show a => Show (Stream a) where
   show = show . take 20 . streamToList
 
 streamRepeat :: a -> Stream a
-streamRepeat x = Cons x . streamRepeat $ x
+streamRepeat a = Cons a . streamRepeat $ a
 
 streamMap :: (a -> b) -> Stream a -> Stream b
 streamMap f (Cons h t) = Cons (f h) (streamMap f t)
@@ -67,5 +68,5 @@ instance Num Matrix where
 fib4 :: Integer -> Integer
 fib4 0 = 0
 fib4 n = get12 $ (Mat4 1 1 1 0)^n
-  where get12 (Mat4 _ x _ _) = x
+  where get12 (Mat4 _ a _ _) = a
 

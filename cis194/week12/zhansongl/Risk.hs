6aae3dcfb9639c952d47d9e9d7c41d125b9affa2
Zhansong Li <lizhansong@hvariant.com>
2019-10-12 02:21:25 +1100

    exercise 5

--- cis194/week12/zhansongl/Risk.hs
+++ cis194/week12/zhansongl/Risk.hs
@@ -4,7 +4,7 @@
 module Risk where
 
 import Control.Monad (replicateM)
-import Data.List (sortBy)
+import Data.List (sortBy, sort, group)
 --import Debug.Trace (trace)
 import System.Random (StdGen)
 
@@ -59,7 +59,38 @@ invade = iterateUntilM battleOver battle
   where battleOver (Battlefield a d) = a <= 2 || d <= 0
 
 successProb :: Battlefield -> Rand StdGen Double
-successProb b = (/) <$> (fromIntegral <$> successCount) <*> pure 1000
-  where rs = replicateM 1000 . invade $ b
+successProb b = (/) <$> (fromIntegral <$> successCount) <*> pure 10000
+  where rs = replicateM 10000 . invade $ b
         successCount = (length . filter winning) <$> rs
         winning (Battlefield a _) = a > 2
+
+cartProd :: [m a] -> [m a] -> [(m a, m a)]
+cartProd o1 o2 = [(x,y) | x <- o1, y <- o2]
+
+outcome :: Int -> [[Int]]
+outcome 0 = []
+outcome 1 = [[1], [2], [3], [4], [5], [6]]
+outcome n = fmap (sortBy $ flip compare) . fmap (uncurry (++)) $ cartProd (outcome 1) (outcome $ n-1)
+
+fightOutcome :: ([Int], [Int]) -> (Int, Int)
+fightOutcome (as, ds) = (aloss, dloss)
+  where r = zipWith (>) as ds
+        dloss = length . filter id $ r
+        aloss = length . filter not $ r
+
+outcomeProb :: Int -> Int -> [(Int, Int, Double)]
+outcomeProb a d = zipWith p (fmap head . group $ r) (fmap length . group $ r)
+  where r = sort . fmap fightOutcome $ cartProd (outcome a) (outcome d)
+        denom = length r
+        p (aloss, dloss) num = (aloss, dloss, fromIntegral num / fromIntegral denom)
+
+exactSuccessProb :: Battlefield -> Double
+exactSuccessProb (Battlefield a d)
+  | a <= 2 = 0
+  | d <= 0 = 1
+  | otherwise = sum
+              . fmap (\(aloss, dloss, prob)
+                      -> (exactSuccessProb $ Battlefield (a - aloss) (d - dloss)) * prob)
+              $ outcomeProb as ds
+  where as = min 3 (a-1)
+        ds = min 2 d
